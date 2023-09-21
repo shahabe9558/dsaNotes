92b9a7bfde670c3b4b8b2420fc9551af18c7122e
shahabe9558 <shahabe9559@gmail.com>
2023-09-21 14:33:49 +0530

    4 sol completed for fibonacci series

--- dp/fibnacci.cpp
+++ dp/fibnacci.cpp
@@ -1,31 +1,79 @@
-
-// 2- Recursion + memoization (top down approach)
+// 4-space optimization
 #include<iostream>
 #include<vector>
 using namespace std;
-int fib(int n, vector<int> &dp)
-{
-    // base case 
-    if(n <= 1)
-    {
-        return n;
-    }
-    if(dp[n] != -1)
-    return dp[n];
-
-    dp[n] = fib(n-1, dp) + fib(n-2, dp);
-    return dp[n];
-}
 int main()
 {
     int n;
     cout <<"Which term do you want ...?" << endl;
     cin >> n;
-    vector<int> dp(n + 1, -1);
-
-    int ans = fib(n, dp);
-    cout << ans;
+    int pre1 = 0;
+    int pre2 = 1;
+    for(int i=2; i<=n; i++)
+    {
+        int curr = (pre1 + pre2) % 1000000007;
+        pre1 = pre2;
+        pre2 = curr;
+    }
+    cout << pre2;
 }
+
+
+
+
+// 3-Tabulation method 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int fib(int n, vector<int> &dp)
+// {
+//     // base case 
+//     dp[0] = 0;
+//     dp[1] = 1;
+//     for(int i =2; i<=n; i++)
+//     {
+//         dp[i] = dp[i-1] + dp[i-2];
+//     }
+//     return dp[n];
+// }
+// int main()
+// {
+//     int n;
+//     cout <<"Which term do you want ...?" << endl;
+//     cin >> n;
+//     vector<int> dp(n + 1, -1);
+//     int ans = fib(n, dp);
+//     cout << ans;
+// }
+
+
+// 2- Recursion + memoization (top down approach)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int fib(int n, vector<int> &dp)
+// {
+//     // base case 
+//     if(n <= 1)
+//     {
+//         return n;
+//     }
+//     if(dp[n] != -1)
+//     return dp[n];
+
+//     dp[n] = fib(n-1, dp) + fib(n-2, dp);
+//     return dp[n];
+// }
+// int main()
+// {
+//     int n;
+//     cout <<"Which term do you want ...?" << endl;
+//     cin >> n;
+//     vector<int> dp(n + 1, -1);
+
+//     int ans = fib(n, dp);
+//     cout << ans;
+// }
 
 
 
@@ -35,7 +83,7 @@ int main()
 
 
 // 1- recursion method 
-#include<iostream>
+// #include<iostream>
 // using namespace std;
 // int fib(int n)
 // {
