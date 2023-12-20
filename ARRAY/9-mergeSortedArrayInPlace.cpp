43d205149587087902c6e1055d652293f053ca3a
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-20 15:48:10 +0530

    :prctice quetion are going on

---
+++ ARRAY/9-mergeSortedArrayInPlace.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+void mergeTwoArray(int arr1[], int n, int arr2[], int m)
+{
+   int left = n -1;
+   int right = 0;
+   while(left >= 0 && right < m)
+   {
+    if(arr1[left] > arr2[right])
+    {
+        swap(arr1[left], arr2[right]);
+        left--;
+        right++;
+
+    }
+    else{
+        break;
+    }
+   }
+   sort(arr1, arr1+n);
+   sort(arr2, arr2+m);
+
+}
+void printarray(int arr3[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout <<arr3[i] <<" ";
+    }
+}
+int main()
+{
+    int arr1[3] = {1,3,5};
+    int arr2[3] = {2,4,6};
+
+    mergeTwoArray(arr1, 3, arr2, 3);   
+
+    printarray(arr1, 3);
+    printarray(arr2, 3);
+}
--- rough.cpp
+++ rough.cpp
@@ -2,9 +2,38 @@
 #include<vector>
 
 using namespace std;
+bool isPrime(int n)
+{
+  if(n == 1)
+  {
+    return true;
+  }
+  // assuming any number can be devide with one that why we started count from 1;
+  int count = 1;
+  for(int i =2; i<n; i++)
+  {
+    if(n % i == 0)
+    {
+      return false;
+    }
+  }
+  count++;
+  // any number is prime only when it has only 2 factor
+  if(count == 2)
+  {
+    return true;
+  }else  return false;
+}
 int main()
 {
-  int a =0;
-  int b = 2;
-  cout << (a ^ b);
+  int n;
+  cout << "Enter the number" << endl;
+  cin >> n;
+  bool ans = isPrime(n);
+  if(ans)
+  {
+    cout << "Number is prime" << endl;
+  }else{
+    cout << "Not Prime" << endl;
+  }
 }
