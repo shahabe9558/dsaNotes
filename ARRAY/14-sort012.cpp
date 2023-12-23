42076d2372a4c95fd7058b9e019ebe3b265bf058
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-23 15:53:14 +0530

    even odd question practice

--- ARRAY/14-sort012.cpp
+++ ARRAY/14-sort012.cpp
@@ -8,6 +8,7 @@ void sort012(int arr[], int size)
     if(arr[i] == 1 && i<j)
     {
       swap(arr[i], arr[i+1]);
+      i++;
     }
     else if(arr[j] == 1 && i<j)
     {
@@ -72,12 +73,35 @@ void sort012m2(int arr[], int n)
     }
 
 }
+void sort012M3(int *arr, int n)
+{
+    int mid = 0;
+    int l = 0;
+    int r = n -1;
+    while(mid <= r)
+    {
+        if(arr[mid] == 0){
+            swap(arr[mid], arr[l]);
+            mid++;
+            l++;
+        }
+        else if(arr[mid] ==  1)
+        {
+            mid++;
+        }
+        else{
+            swap(arr[mid], arr[r]);
+            r--;
+        }
+    }
+}
 int main()
 {
     int arr[6] = {2,0,2,1,1,0};
 
-    sort012m2(arr, 6);
+    // sort012m2(arr, 6);
 
-    // sort012(arr, 9);
+    // sort012(arr, 6);
+    sort012M3(arr, 6);
     printArray(arr, 6); 
 }
--- BASIC/1-primeNumber.cpp
+++ BASIC/1-primeNumber.cpp
@@ -1,7 +1,9 @@
 // Isko aur optimize karna hai 
 
 #include<iostream>
+#include<cmath>
 using namespace std;
+// time complexity is o(n);
 bool isPrime(int n)
 {
     for(int i = 2; i < n; i++)
@@ -14,12 +16,22 @@ bool isPrime(int n)
     return true;
 
 }
+// best tC from above
+bool isPrimeM2(int n){
+    for(int i =2; i <=sqrt(n); i++){
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
     cout <<"Enter the number" << endl;
     int n;
     cin >> n;
-    bool sol = isPrime(n);
+    bool sol = isPrimeM2(n);
     if(sol)
     cout << "Number is prime" << endl;
     else
