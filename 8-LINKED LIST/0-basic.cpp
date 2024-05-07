4357f2319209f4c7a245809ab62f78c3cd187fba
Shahabenawaz Alam <shahabe9559@gmail.com>
2024-05-08 02:04:09 +0530

    graph revised doen

--- 8-LINKED LIST/0-basic.cpp
+++ 8-LINKED LIST/0-basic.cpp
@@ -23,172 +23,144 @@
 //     }
 // };
 
-//    void print(Node* &head) {
-//     if(head == NULL) {
-//         cout << "List is empty "<< endl;
-//         return ;
-//     }
-//     Node* temp = head;
-//     while(temp != NULL ) {
-//         cout << temp -> data << " ";
-//         temp = temp -> next;
-//     }
-//     cout << endl;
-// }
-//    void insertAtTail(Node* &tail, int d)
-//    {
-//     // creating a Node
-//     Node* temp = new Node(d);
-//     tail->next = temp;
-//     tail = temp;
-//    }
 
-//    void inserAtHead(Node* &head, int d)
-//    {
-//     // Creating new node
-//     Node* temp = new Node(d);
-//     temp -> next = head;
-//     head = temp;
-//    }
-
-//    void insertAtPosition(Node* & head, int position, int d) {
-//     //insert at Start
-//     // if(position == 1) {
-//     //     insertAtHead(head, d);
-//     //     return;
-//     // }
-
-//     Node* temp  = head;
-//     int cnt = 1;
-
-//     while(cnt < position-1) {
-//         temp = temp->next;
-//         cnt++;
-//     }
-
-//     //inserting at Last Position
-//     // if(temp -> next == NULL) {
-//     //     insertAtTail(tail,d);
-//     //     return ;
-//     // }
-
-//     //creating a node for d
-//     Node* nodeToInsert = new Node(d);
-
-//     nodeToInsert -> next = temp -> next;
-
-//     temp -> next = nodeToInsert;
-//     cout <<endl;
-//     // cout << "Head is = " <<  head->data;
-// }
-  
-//   void deleteNode(int position, Node* &head, Node* &tail)
-// {
-
-//   // deleting first Node or starting node
-//   // cout << "Head = " << head->data << endl;
-//   // cout << "tail = " << tail->data << endl;
-
-//   if(position == 1)
-//   {
-  
-
-//     Node* temp = head;
-//     head = head->next;
-
-//     // doing memory free 
-//     temp->next = NULL;
-//     delete temp;
-
-//   }
-//    // deleting last or any node from the linked list  
-//    Node* curr = head;
-//    Node* pre = NULL;
-
-//    int cnt = 1;
-//    while(cnt < position)
-//    {
-//     pre = curr;
-//     curr = curr->next;
-//     cnt++;
-//    }
-//    if(position == 5)
-//    {
-//     pre->next = curr->next;
-//     tail = pre;
-//     curr->next = NULL;
-//     delete curr;
-
-//    }
-//    else{
-    
-//       pre->next = curr->next;
-//       curr->next = NULL;
-//       delete curr;
-
-   
-// }
-// int main()
-// {
-//     Node* node1 = new Node(12);
-//     Node* head = node1;
-//     Node* tail = node1;
-//     insertAtTail(tail, 15);
-//     insertAtTail(tail, 19);
-//     inserAtHead(head, 10);
-//     insertAtPosition(head, 3, 20);
-//     insertAtPosition(head, 2, 21);
-//     print(head);
-//     deleteNode(2, head, tail);
-//     print(head);
-
-//     cout << "head is pointing = " << head->data << endl;
-//     cout << "tail is pointing = " << tail->data << endl;
-
-
-
-//     // inserAtHead(head, 31);
-//     // insertAtTail(tail, 50);
-//     // print(head);
-    
-    
-
-//     // print(head);
-//     // cout << "Node data is = " << node1->data << endl;
-//     // cout << "Node next is = " << node1->next << endl;
-
-//     // cout << "node1 is a = " << node1 << endl;
-//     // inserAtHead(head, 15);
-//     // inserAtHead(head, 34);
-// }
-
-
-
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Node{
   public :
   int data;
-  int *next;
+  Node *next;
+  // definning the constructor 
   Node(int d)
   {
     this->data = d;
     this->next = NULL;
   }
+  // destructor 
   ~Node()
   {
-    delete next;
-    cout << "Node deleted successfully" << endl;
+    int value = this -> data;
+    cout << "this->data = " << this->data << endl;
+    if(this->next != NULL) {
+        delete next;
+        this->next = NULL;
+    }
+    cout << "memory is free for node with data " << value << endl;
   }
 };
+void print(Node * head)
+{
+  if(head == NULL)
+  {
+    cout << "List is empty" << endl;
+    return;
+  }
+  Node * temp = head;
+  while(temp != NULL)
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+  cout << endl;
+}
+void insertAtHead(Node * &head, int d)
+{
+  Node *temp = new Node(d);
+  temp->next = head;
+  head = temp;
+}
+void insertAtTail(Node * &tail, int d)
+{
+  Node * temp  = new Node(d);
+  tail->next = temp;
+  tail = temp;
+}
+void insertAtPos(Node * &head, int position, int d)
+{
+  // agar head par laganan ho to 
+  if(position == 1)
+  {
+    insertAtHead(head, d);
+    return;
+  }
+  // pahle to positiontak pahucna hoga na 
+  Node * temp = head;
+  int cnt = 1;
+  while(cnt < position - 1)
+  {
+    temp = temp->next;
+    cnt++;
+  }
+  if(temp->next == NULL)
+  {
+    insertAtTail(temp, d);
+    return;
+  }
+  // pos par insert kar do 
+  Node* nodeToinsert = new Node(d);
+  nodeToinsert->next = temp->next;
+  temp->next = nodeToinsert;
+}
+void deleteNode(Node * &head, Node * &tail, int position)
+{
+  // agar position one hai to 
+  if(position == 1)
+  {
+    Node * temp = head;
+    head = head->next;
+    temp->next = NULL;
+    // doint memory free 
+    delete temp;
+    return;
+  }
+  // if it is last node  or any
+  Node * curr = head;
+  Node * pre = NULL;
+  int cnt = 1;
+  while(cnt < position)
+  {
+    pre = curr;
+    curr = curr->next;
+    cnt++;
+  }
+  // means on last pos 
+  if(curr->next == NULL)
+  {
+    pre->next = curr->next;
+    tail = pre;
+    curr->next = NULL;
+    delete curr;
+  }
+  else
+  {
+    pre->next = curr->next;
+    curr->next = NULL;
+    delete curr;
+  }
+}
 int main()
 {
-  Node node(12);
-  cout << node.data << endl;
-  cout << node.next << endl;
+  Node *node1 = new Node(12);
+  Node* head = node1;
+  Node * tail = node1;
+  insertAtHead(head, 10);
+  insertAtHead(head,8);
+  insertAtHead(head,6);
+  insertAtTail(tail, 14);
+  insertAtPos(head, 3, 9);
+  insertAtPos(head, 1, 4);
+  insertAtPos(head, 8, 16);
+  deleteNode(head, tail, 4);
+  deleteNode(head, tail, 7);
+  deleteNode(head, tail, 1);
+  deleteNode(head, tail, 1);
+  print(head);
+  cout << "Head is at " << head->data << endl;
+  cout << "Tail is at " << tail->data << endl;
+  cout << "Head next is " << head->next << endl;
+  cout << "Tail next is " << tail->next << endl;
 }
 
 
 
-
+ 
