0c26f08986dbc6c4f3326dc594b8f6017a136555
Shahabenawaz Alam <shahabe9559@gmail.com>
2024-05-08 19:34:15 +0530

    queue revision

--- 10-QUEUE/0-implmnt.cpp
+++ 10-QUEUE/0-implmnt.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include <deque>
 using namespace std;
 
 // it follow FIFO order 
@@ -12,24 +13,43 @@ using namespace std;
 //    i.e pop_front, push_back, push_front hoga
 // 5- doubly ended queue 
 //    i.e push_back, push_front, pop_back, pop_front hoga
-
-
-
 int main()
 {
+    // queue<int> q;
+    // q.push(1);
+    // q.push(2);
+    // q.push(3);
+    // q.push(4);
+    // cout << "size of queue = " << q.size() << endl;
+    // q.pop();
+    // cout << "size of queue = " << q.size() << endl;
+    // cout << "Empty or not = " << q.empty() << endl;
+    // cout << "front is = " << q.front();
     queue<int> q;
     q.push(1);
     q.push(2);
     q.push(3);
     q.push(4);
-
-    cout << "size of queue = " << q.size() << endl;
-
-    q.pop();
-
-    cout << "size of queue = " << q.size() << endl;
-
-    cout << "Empty or not = " << q.empty() << endl;
-
-    cout << "front is = " << q.front();
+    q.push(5);
+    cout << "Front element is " << q.front() << endl;
+    cout << "Back element is " << q.back() << endl;
+    cout << "The size is " << q.size() << endl;
+    cout << "Is empty " << q.empty() << endl;
+    while(!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    deque<int> d;
+    d.push_back(1);
+    d.push_back(2);
+    d.push_back(3);
+    d.push_back(4);
+    d.push_back(5);
+    while(!d.empty())
+    {
+        cout << d.front() << " ";
+        d.pop_front();
+    }
 }
--- 10-QUEUE/1-impUsingClass.cpp
+++ 10-QUEUE/1-impUsingClass.cpp
@@ -1,73 +1,73 @@
 #include<iostream>
 using namespace std;
-class QUEUE{
-    public:
-    int rear;
-    int front;
-    int * arr;
-    int size;
-
-    // constructor 
-    QUEUE()
+class Queue{
+  public:
+  int front;
+  int rear;
+  int size;
+  int *arr;
+  Queue()
+  {
+    size = 1000;
+    arr = new int[size];
+    rear = 0;
+    front = 0;
+  }
+  bool isEmpty()
+  {
+    if(front == rear)
     {
-        size = 10001;
-        arr = new int[size];
-        front = 0;
-        rear = 0;
+      return true;
     }
-    bool isEmpty()
-    {
-       if(front == rear)
-       return true;
-       else
-       return false;
+    else{
+      return false;
     }
-    void enqueue(int num)
+  }
+  void enQueue(int num)
+  {
+    if(rear == size)
     {
-        if(rear == size)
-        {
-          cout <<"Queue is full " << endl;
-        }
-        else{
-            arr[rear] = num;
-            rear++;
-        }
+      cout << "Stack is full" << endl;
     }
-    void dequeue()
+    else{
+      arr[rear] = num;
+      rear++;
+    }
+  }
+  void deQueue()
+  {
+    if(front == rear)
     {
+      cout << "Queue is empty" << endl;
+    }
+    else{
+      arr[front] = -1;
+      front++;
       if(front == rear)
       {
-        cout << "Queue is empty" << endl;
-
-      }
-      else{
-        arr[front] = -1;
-        front++;
-        if(front == rear)
-        {
-            front = 0;
-            rear = 0;
-        }
+        front = 0;
+        rear = 0;
       }
     }
-    int front1()
+  }
+  int peak()
+  {
+    if(front == rear)
     {
-        if(front == rear)
-        {
-            cout << "Kuch nahi hai" << endl;
-        }
-        else
-        { 
-            return arr[front];
-        }
+      cout << "Queue is empty" << endl;
     }
+    else{
+      return arr[front];
+    }
+  }
 };
 int main()
 {
-   QUEUE q;
-   q.enqueue(2);
-   q.enqueue(2);
-   q.dequeue();
-   cout << q.front1();
-   cout << "empty = " << q.isEmpty();
+  Queue q;
+  q.enQueue(1);
+  q.enQueue(2);
+  q.enQueue(3);
+  q.deQueue();
+  cout << q.peak() << endl;
+  cout << q.isEmpty() << endl;
 }
